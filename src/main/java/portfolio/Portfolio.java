e8742a9ae0d2eb0337fdc59e9680d48ac85f968e
Brogle Flavia <flavia.brogle@students.fhnw.ch>
2020-03-02 11:00:11 +0100

    Implement Portfolio according to tests Uncommented test lines in PortfolioTest

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,46 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+import portfolio.investments.Investment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Portfolio<T extends Investment> {
+    private List<T> investments = new ArrayList<>();
+
+    public boolean contains(T investment) {
+        String title = investment.getTitel();
+        for (T i : investments) {
+            if (i.getTitel().equals(title)) return true;
+        }
+        return false;
+    }
+
+    public void buy(T investment) {
+        String title = investment.getTitel();
+        T i = getShare(investment.getTitel());
+        if (i == null) investments.add(investment);
+        else i.setCount(i.getCount() + investment.getCount());
+    }
+
+    public void sell(String investmentName, int count) {
+        Investment i = getShare(investmentName);
+        if (i == null) return;
+        double newCount = i.getCount() - count;
+        if (newCount <= 0) {
+            investments.remove(i);
+            i.setCount(0);
+        } else {
+            i.setCount(newCount);
+        }
+    }
+
+    public T getShare(String investmentName) {
+        for (T investment : investments) {
+            if (investment.getTitel().equals(investmentName)) {
+                return investment;
+            }
+        }
+        return null;
+    }
 }
